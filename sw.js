52079f5d598062a33bbf4cfa59f6b20bcae0fae4
joonsoo97-alt <joonsoo97@gmail.com>
2026-08-25 09:06:16 +0900

    Update service worker cache name and asset paths

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "pregnancy-diary-v1";
+const CACHE_NAME = "pregnancy-diary-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
